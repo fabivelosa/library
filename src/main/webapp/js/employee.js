65ff0b9ecf7b0aeaa8316755b01d63f9b8a2a1be
FabiVelosa <fabivelosa@gmail.com>
2020-04-02 23:55:12 +0100

    update classes fixed, register as a member, register customer to class

--- src/main/webapp/js/employee.js
+++ src/main/webapp/js/employee.js
@@ -3,24 +3,22 @@
  */
 var rootURL = "http://localhost:8080/library/rest";
 
-var dataClasses;
-var dataUsers;
-var dataRegistration;
+$(document).ready(function() {
+	initEmployeePage();
+});
 
 $(function() {
 	$("#tabs").tabs({
 		activate : function(event, ui) {
-
-				if (ui.newTab.index() == 1) {
-					findAllUsers();
-				}else if(ui.newTab.index() == 2) {
-				     	findAllRegistration(1);
-				} else if (ui.newTab.index() == 3) {
-					findAllClasses();
-				}else if(ui.newTab.index() ==5) {
-					//renderGrid(dataClasses);
-					findAttendance() ;
-			} 
+			if (ui.newTab.index() == 1) {
+				findAllUsersMember();
+			} else if (ui.newTab.index() == 2) {
+				findAllUsers();
+			} else if (ui.newTab.index() == 3) {
+				findAllClasses();
+			} else if (ui.newTab.index() == 5) {
+				findAttendance();
+			}
 		}
 	});
 });
@@ -31,10 +29,26 @@ function findAllClasses() {
 		url : rootURL + '/classes',
 		dataType : "json",
 		success : function(data) {
-			dataClasses = data;
-			renderDTClasses(dataClasses);
-			}
-		
+			renderDTClasses(data);
+		}
+	});
+}
+
+function findClasseById(id) {
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/classes/query?user=0&class=' + id, // classes/query?user=0&class=9
+		dataType : "json",
+		success : function(data) {
+			$('#classId').val(data[0].class_id);
+			$('#duration').val(data[0].class_duration);
+			$('#title').val(data[0].class_title);
+			$('#category').val(data[0].class_category);
+			$('#start_class').val(data[0].class_start);
+			$('#slot').val(data[0].class_slot);
+			$('#fee').val(data[0].class_fee);
+		}
+
 	});
 }
 
@@ -44,8 +58,7 @@ function findAllRegistration(classID) {
 		url : rootURL + '/registration/query?user=0&class=' + classID,
 		dataType : "json",
 		success : function(data) {
-			dataRegistration = data;
-			//renderDTRegistration(data);
+			// renderDTRegistration(data);
 		}
 	});
 }
@@ -56,8 +69,19 @@ function findAllRegistration(userID) {
 		url : rootURL + '/registration/query?user=' + userID + '&class=0',
 		dataType : "json",
 		success : function(data) {
-			dataRegistration = data;
-			//renderDTRegistration(data);
+			// renderDTRegistration(data);
+		}
+	});
+}
+
+function findAllUsersMember() {
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/membership',
+		dataType : "json",
+		success : function(data) {
+			renderDTUsers(data);
+
 		}
 	});
 }
@@ -65,34 +89,108 @@ function findAllRegistration(userID) {
 function findAllUsers() {
 	$.ajax({
 		type : 'GET',
-		url : rootURL + '/membership',
+		url : rootURL + '/user',
 		dataType : "json",
 		success : function(data) {
-			dataUsers = data;
-			renderDTUsers(dataUsers);
+			renderUserList(data);
 
 		}
 	});
 }
 
-function findAttendance() {
-	console.log("atendance");
+function findUserByName(name) {
 	$.ajax({
 		type : 'GET',
-		url : rootURL + '/classes/' + classId,
+		url : rootURL + '/user/search/' + name,
+		dataType : "json",
+		success : function(user) {
+			if (user.length == 0) {
+				alert("Not found! Please search by Last Name!");
+				$('#userList').remove();
+				findAllUsers();
+			} else {
+				$("#userList").remove;
+				renderUserList(user);
+			}
+		}
+	});
+}
+
+function findUserClasses(userId) {
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/classes/query?user=' + userId + '&class=0',
+		dataType : "json",
+		success : renderUserClasses
+	});
+}
+
+function findAttendance() {
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/classes/' + 1,
+
 		dataType : "json",
 		success : function(data) {
-			dataClasses = data;
-			renderGrid(dataClasses);
-			}
-		
+			renderGrid(data);
+		}
+
+	});
+}
+
+var renderUserList = function(users) {
+	console.log(users);
+	$("#userList").empty();
+	$.each(users, function(index, user) {
+		$("#userList").append(
+				'<li><a href="#" id="' + user.userId + '">' + user.firstname
+						+ ' ' + user.lastname + '</a></li>');
+
+	})
+}
+
+var renderUserClasses = function(data) {
+
+	if ($.fn.dataTable.isDataTable('#registerclasses')) {
+		var table = $('#registerclasses').DataTable();
+		table.clear();
+		table.destroy();
+	}
+	$
+			.each(
+					data,
+					function(index, c) {
+						$('#registerclassesbody')
+								.append(
+										'<tr><td>'
+												+ c.class_title
+												+ '</td><td>'
+												+ c.class_category
+												+ '</td><td>'
+												+ c.class_slot
+												+ '</td><td>'
+												+ c.class_fee
+												+ '</td><td>'
+												+ c.class_start
+												+ '</td><td>'
+												+ c.class_duration
+												+ '</td><td>'
+												+ (c.registrationId > 0 ? '<a href="#" data-identity="'
+														+ c.registrationId
+														+ '" data-toggle="modal" data-target="#class-unreg-modal">Unregister</a>'
+														: '<a href="#" data-identity="'
+																+ c.class_id
+																+ '" data-toggle="modal" data-target="#class-reg-modal">Register</a>')
+												+ '</td></tr>');
+					});
+
+	$('#registerclasses').DataTable({
+		ordering : false
 	});
 }
 
 function renderGrid(data) {
-	// $('#classes-item').remove();
-	console.log("grid");
-	console.log(data);
+	console.log($('#classes-item').length);
 	$(data).each(function(i, classes) {
 		append(classes);
 	});
@@ -115,6 +213,11 @@ function append(classes) {
 }
 
 function renderDTClasses(data) {
+	if ($.fn.dataTable.isDataTable('#table_id-3')) {
+		var table = $('#table_id-3').DataTable();
+		table.clear();
+		table.destroy();
+	}
 	$('#table_id-3')
 			.DataTable(
 					{
@@ -136,7 +239,7 @@ function renderDTClasses(data) {
 						}, {
 							"data" : "class_duration"
 						}, {
-							"data" : "null"
+							"data" : "class_id"
 						}, ],
 						"columnDefs" : [
 								{// title
@@ -195,45 +298,130 @@ function renderDTClasses(data) {
 									orderable : false,
 									render : function(data, type, full_row,
 											meta) {
-										return '<div style="display:block">'
-												+ '<button onclick="edit_action(this, '
-												+ full_row.id
-												+ ')" type="button" class="edit_action btn btn-warning btn-xs" data-toggle="modal" data-target="#class-update-modal" style="margin:3px"><i class="fa fa-edit"></i> Edit </button>'
-												+ '</div>';
-										// '<button id="editBtn" class="btn
-										// btn-wrang btn-flat edit"
-										// name="editBtn"
-										// type="button">Edit</button>';
-										// <a href="#">Unregister</a>' : '<a
-										// href="#" data-toggle="modal"
-										// data-target="#class-update-modal">Register</a>')
+
+										return '<a href="#" data-identity="'
+												+ data
+												+ '" data-toggle="modal" data-target="#class-update-modal">Edit</a>';
 
 									}
 								} ],
 					});
-	
-	$('#edit_action').on('click', function(e) {
-		e.preventDefault();
-		// $.ajax({
-		// url: "{{ route('admin.book_languages.edit') }}",
-		// data: {
-		// 'item_id': $('#item_id').val(),
-		// 'language_name': $('#language_name').val(),
-		// '_token': "{{ csrf_token() }}"
-		// },
-		// type: "POST",
-		// success: function (response) {
-		// $('#modal_edit').modal('hide');
-		// DataTable.ajax.reload(null, false);
-		// console.log(data.message);
-		// }
-		// })
+}
+
+function initEmployeePage() {
+	$('#class-update-modal').on('hidden.bs.modal', function() {
+		console.log('hide.bs.modal');
+		$('#classId').val("");
+		$('#duration').val("");
+		$('#title').val("");
+		$('#category').val("");
+		$('#start').val("");
+		$('#slot').val("");
+		$('#fee').val("");
 	});
 
-	$('#class-update-modal').on('hidden.bs.modal', function() {
-		$('#class_id').val(0);
-		// $('#language_name').val("");
+	$('#class-update-modal').on('show.bs.modal', function(event) {
+		console.log('show.bs.modal');
+		var actionLink = $(event.relatedTarget);
+		var classId = actionLink.data('identity');
+		console.log('classId :' + classId);
+		findClasseById(classId);
+
 	});
+
+	$('#btn-save').on('click', function(e) {
+		// e.preventDefault();
+		var classId = $('#classId').val();
+		console.log('click' + classId);
+		console.log(formToJSON());
+		$.ajax({
+			type : 'PUT',
+			contentType : 'application/json',
+			url : rootURL + '/classes/' + classId,
+			dataType : "json",
+			data : formToJSON(),
+			success : function(response) {
+				findAllClasses();
+				$('#class-update-modal').modal("hide");
+
+			}
+		})
+	});
+
+	$('#success_message').hide();
+
+	$('#btnSaveUser').click(function() {
+		if ($('#userId').val() == '')
+			addUser();
+		return false;
+	});
+
+	$(document).on("click", "#userList a", function() {
+		findUserClasses(this.id);
+	});
+	$(document).on("click", "#btnSearch", function() {
+		findUserByName($('#searchKey').val())
+	});
+}
+
+var formToJSON = function() {
+	return JSON.stringify({
+		"class_id" : $('#classId').val(),
+		"class_duration" : $('#duration').val(),
+		"class_title" : $('#title').val(),
+		"class_category" : $('#category').val(),
+		"class_start" : $('#start_class').val(),
+		"class_slot" : $('#slot').val(),
+		"class_fee" : $('#fee').val()
+	});
+};
+
+var addUser = function() {
+	console.log('addUser');
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : rootURL + '/user',
+		dataType : "json",
+		data : UserformToJSON(),
+		success : function(data, textStatus, jqXHR) {
+			alert('User created successfully');
+			$('#userId').val(data.id);
+			// findAll();
+			$('#success_message').show();
+			clearForm();
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('addUser error: ' + textStatus);
+		}
+	});
+};
+
+var UserformToJSON = function() {
+	return JSON.stringify({
+		"category" : $('#cmbCategory').val(),
+		"birth_date" : $('#dateofbirth').val(),
+		"eircode" : $('#eircode').val(),
+		"email" : $('#email').val(),
+		"college_name" : $('#college').val(),
+		"firstname" : $('#first_name').val(),
+		"lastname" : $('#last_name').val(),
+		"mobileTel" : $('#contact_no').val(),
+		"addressName" : $('#address_name').val(),
+		"addressStreet" : $('#address').val(),
+		"addressTown" : $('#town').val(),
+		"addressCounty" : $('#county').val()
+	});
+};
+
+var clearForm = function() {
+
+	$('#cmbCategory').val(""), $('#dateofbirth').val(""),
+			$('#eircode').val(""), $('#email').val(""), $('#college').val(""),
+			$('#first_name').val(""), $('#last_name').val(""), $('#contact_no')
+					.val(""), $('#address_name').val(""),
+			$('#address').val(""), $('#town').val(""), $('#county').val("")
+
 }
 
 function renderDTUsers(data) {
@@ -304,14 +492,13 @@ function renderDTUsers(data) {
 									targets : 5,
 									className : "dt-center",
 									render : function(data, type, full, meta) {
-							
-										
+
 										var button;
 										if (data > 0) {
 											button = '<button id="deleteBtn"  class="btn btn-info btn-flat delete"  name="deleteBtn" type="button">UnRegister</button>';
 										} else {
-											button= '<button id="saveBtn"  class="btn btn-info btn-flat edit"  name="saveBtn" type="button">Register</button>';
-											
+											button = '<button id="saveBtn"  class="btn btn-info btn-flat edit"  name="saveBtn" type="button">Register</button>';
+
 										}
 										return button;
 									}
@@ -387,18 +574,16 @@ function renderDTRegistration(data) {
 									targets : 5,
 									className : "dt-center",
 									render : function(data, type, full, meta) {
-							
-										
+
 										var button;
 										if (data > 0) {
 											button = '<button id="deleteBtn"  class="btn btn-info btn-flat delete"  name="deleteBtn" type="button">UnRegister</button>';
 										} else {
-											button= '<button id="saveBtn"  class="btn btn-info btn-flat edit"  name="saveBtn" type="button">Register</button>';
-											
+											button = '<button id="saveBtn"  class="btn btn-info btn-flat edit"  name="saveBtn" type="button">Register</button>';
+
 										}
 										return button;
 									}
 								} ],
 					});
 }
-
