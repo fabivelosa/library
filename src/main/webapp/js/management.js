8ba53d4c9f1ecf95ea17d26d4314239f56720208
Conor <coj265@gmail.com>
2020-04-08 20:43:19 +0100

    Management data table and edit function

--- src/main/webapp/js/management.js
+++ src/main/webapp/js/management.js
@@ -1,57 +1,33 @@
-var classesURL = "http://localhost:8080/library/rest/classes";
+var rootURL = "http://localhost:8080/library/rest/classes";
 
-
-
-var addClass = function () {
-	console.log('addClass');
-	$.ajax({
-		type: 'POST',
-		contentType: 'application/json',
-		url: classesURL,
-		dataType: "json",
-		data: formToJSON(),
-		success: function(data, textStatus, jqXHR){
-			alert('Class created successfully');
-//			$('#btnDelete').show();
-			$('#class_title').val(data.class_title);
-                        findAll();
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('addClass error: ' + textStatus);
-		}
-	});
-};
-
-
-var updateClass= function () {
-	console.log('updateClass');
-	$.ajax({
-		type: 'PUT',
-		contentType: 'application/json',
-		url: rootURL + '/' + $('#class_title').val(),
-		dataType: "json",
-		data: formToJSON(),
-		success: function(data, textStatus, jqXHR){
-			alert('Class updated successfully');
-                         findAll();
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('updateClass error: ' + textStatus);
-		}
-	});
-};
-
-var renderList = function(data){
-list = data.classes;
-console.log("classes");
-$.each(list, function(index, classes)	{
+$(document).ready(function() {
+	findClasses();
 	
-	$('#table_body').append('<tr><td>'+classes.class_title+'</td><td>'+classes.class_category+'</td><td>'+classes.class_slot+'</td><td>'+classes.class_fee+'</td><td>'+classes.class_start+'</td><td>'+classes.class_duration+'</td></tr>');
+	$('#btn-save').click(function(){
+		console.log('Button save clicked');
+		
+		var formData = formToJSON();
+		
+		$.ajax({
+    		type: 'POST',
+    		contentType: 'application/json',
+    		url: rootURL,
+    		data: formData,
+    		success: function(){
+    			console.log('successfully saved');
+    			findClasses();
+    			$('#class-modal').modal("hide");
+    		}
+    	});
+	})
+	$('#classes_table_body a').click(function(){
+		deleteClass();
+		return false;
+	});
+	
 });
-}
 
 var formToJSON = function () {
-	var class_title = $('#class_title').val();
 	return JSON.stringify({
 		"class_title": $('#class_title').val(), 
 		"class_category": $('#class_category').val(), 
@@ -59,29 +35,51 @@ var formToJSON = function () {
 		"class_fee": $('#class_fee').val(),
 		"class_start": $('#class_start').val(),
 		"class_duration": $('#class_duration').val(),
-		});
+	});
 };
 
-$(document).ready( function () {
-    $('#classes_table').DataTable();
-    
-    $('#btnAdd').click(function() {
-		newClass();
-		return false;
+function findClasses(){
+	console.log('findClasses');
+	$.ajax({
+		type: 'GET',
+		url: rootURL,
+		dataType: "json",
+		success: renderClasses
+	});
+}
+
+var deleteClass = function(){
+	$.ajax({
+		type: 'DELETE',
+		contentType: 'application/json',
+		url: rootURL + '/' + id,
+		success: function(){
+			alert('Class deleted successfully');
+			findClasses();
+		}
+	});
+};
+
+function renderClasses(data){
+	if ($.fn.dataTable.isDataTable('#classes_table')) {
+		var table = $('#classes_table').DataTable();
+		table.clear();
+		table.destroy();
+	}
+	$.each(data, function(index, c){
+		$('#classes_table_body').append('<tr><td>' + c.class_title + '</td><td>'
+			+ c.class_category + '</td><td>'
+			+ c.class_slot + '</td><td>'
+			+ c.class_fee + '</td><td>'
+			+ c.class_start + '</td><td>'
+			+ c.class_duration + '</td><td>'
+			+ '<a href="#" data-identity="' + c.class_id + '" data-toggle="modal" data-target="#class-modal">Edit</a>'
+			+ ' | <a href="#" data-identity="' + c.class_id + '" data-toggle="modal" data-target="#class-delete-modal">Delete</a>'
+			+ '</td></tr>'
+		);
 	});
 
-	$('#btnSave').click(function() {
-		if ($('#classes_title').val() == '')
-			addClass();
-		else
-			updateClass();
-		return false;
-	});
+	$('#classes_table').DataTable();
+}
 
-	$('#btnDelete').click(function() {
-		deleteClass();
-		return false;
-	});
-    
-});
 
